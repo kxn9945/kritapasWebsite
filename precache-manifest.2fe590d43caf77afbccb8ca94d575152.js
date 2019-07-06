eafc0e6e0f562e3614a484e765c888803d88eecb
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-07-06 00:05:06 -0400

    Updates

---
+++ precache-manifest.2fe590d43caf77afbccb8ca94d575152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f2ec0cdbad392a6a9e404d15e4f158",
+    "url": "/kritapasWebsite/index.html"
+  },
+  {
+    "revision": "cb121a8e79abdf120ece",
+    "url": "/kritapasWebsite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/kritapasWebsite/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "cb121a8e79abdf120ece",
+    "url": "/kritapasWebsite/static/js/main.128bbe5e.chunk.js"
+  },
+  {
+    "revision": "81a58b6d12e0e873a6da",
+    "url": "/kritapasWebsite/static/js/runtime~main.af174ced.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kritapasWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
